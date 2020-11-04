7f857327ccfcb480f1f25a9533ee1d6a768de01d
Sogolumbo <33571916+Sogolumbo@users.noreply.github.com>
2020-11-04 18:47:21 +0100

    Remove duplicate newlines

--- spotify-backup.py
+++ spotify-backup.py
@@ -186,7 +186,7 @@ def main():
 			for playlist in playlists:
 				f.write(playlist['name'] + '\r\n')
 				for track in playlist['tracks']:
-					f.write('{name}\t{artists}\t{album}\t{uri}\r\n'.format(
+					f.write('{name}\t{artists}\t{album}\t{uri}\n'.format(
 						uri=track['track']['uri'],
 						name=track['track']['name'],
 						artists=', '.join([artist['name'] for artist in track['track']['artists']]),
